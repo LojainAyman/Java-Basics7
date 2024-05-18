e4407173c659d63bff8e86e14d03ff4205ea09f6
LojainAyman <lojainalmfleh@gmail.com>
2024-05-18 20:25:12 +0300

    update

---
+++ src/Java_toturial.java
@@ -0,0 +1,118 @@
+
+public class Java_toturial {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+
+//		System.out.println(Math.pow(5, 2));
+//		System.out.println(Math.abs(-5));
+//		System.out.println(Math.abs(-5));
+		
+	//	-------------------------
+//		int myFirstNumber = Math.abs(-5);
+//		
+//		double mySecondNumber = Math.abs(-5);
+//		
+//		System.out.println(myFirstNumber);
+//		
+//		System.out.println( mySecondNumber);
+//		-----------------------------
+		
+//		int myInt = 9 ;
+//		double myDouble = myInt ; 
+//		System.out.println( myDouble);
+//		System.out.println(myInt);
+//		-------------------
+
+//		int myInt = 10 ; 
+//		System.out.println(myInt%3);
+//		---------------------
+
+	//	int myInt = 10 ;
+		
+		
+		
+//		
+//		System.out.println(myInt++);
+//		System.out.println(myInt);
+//		
+//		System.out.println(16%10);
+		//-------
+//	System.out.println(++myInt + myInt++);	
+//	System.out.println(myInt);
+//		
+		
+//	----------------------------------	
+		
+//	String myName = "lojain";
+//		String myLastName = "Ayman";
+//		
+//		System.out.println(myName.charAt(3));
+//		
+//		System.out.println(myName.contains("a"));
+//		
+//		System.out.println(myName+myLastName);
+//		
+//		System.out.println(myName.contentEquals("lojains"));
+//		
+//		System.out.println(myName.length()+6);
+//		
+//		System.out.println(myName.valueOf(3));
+//		
+//		 System.out.println(myName.toUpperCase());
+//		
+//		 System.out.println(Math.PI);
+//		-----------------
+		 
+
+	//	    System.out.println(Math.round(55.1));
+//	    System.out.println(Math.round(55.9));
+//	    ----------------
+//	    System.out.println(Math.ceil(55.1));
+//	    System.out.println(Math.ceil(55.9));
+//	    --------------
+//	    System.out.println(Math.floor(55.1));
+//		 System.out.println(Math.floor(55.9));
+//	    
+//	  ------------------
+//
+// System.out.println( Math.random()); // numbers under 1
+//	    
+ // System.out.println( Math.random()*10); //numbers above 10
+//	  
+//	 -------------
+//	    
+  //int a = (int) Math.random();
+//	 
+//
+//	    System.out.println( a);	  
+	//    -----------------------------
+	//   double a =  Math.random()*10;
+	  // System.out.println( a);  
+//	   ---------
+//	int myInt = (int) a ; 
+//
+//   System.out.println( myInt);  
+//-----------
+		
+//   Integer.toString((int) a);
+// System.out.println( myInt);  
+//		--------------
+//		String myAge = "29";
+//		System.out.println( myAge+10);
+//		  -----------
+//		  
+//	 double ssss = Math.random()*1000;
+// System.out.println( ssss); 
+//		 
+		
+		String myAge = "29"; // Very important to work
+		int myUpdateAge = Integer.parseInt(myAge);
+		
+		  System.out.println( myUpdateAge+10); 
+		  
+	//---------------------
+
+	}
+
+}
